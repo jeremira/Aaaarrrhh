48712ab86c1ec67f02f73006d5290be3b098a273
jeremira <jmiraille@gmail.com>
2018-07-28 11:27:19 +0200

    add sad path spec edit/create

--- spec/features/pirates_spec.rb
+++ spec/features/pirates_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Pirates", type: :feature do
   let(:pirate1) {create :pirate}
   let(:pirate2) {create :pirate}
 
-  scenario "I can see all me pirates" do
+  scenario "I see all me pirates" do
     pirate1
     pirate2
     visit root_path
@@ -13,25 +13,87 @@ RSpec.feature "Pirates", type: :feature do
     expect(page).to have_content pirate2.name
   end
 
-  scenario "I can create a new pirate" do
-    visit root_path
-    fill_in :pirate_name, with: "new_pirate"
-    select 5, from: :pirate_hp
-    select 5, from: :pirate_dmg
-    click_button "Get me a new pirate !"
-    expect(page).to have_content "A new pirate has been add t' th' crew !"
-    expect(Pirate.find_by(name: "new_pirate")).to be_an Pirate
+  describe "I create a new pirate" do
+    before(:each) {visit root_path}
+
+    context "with valid name" do
+      before :each do
+        fill_in :pirate_name, with: "new_pirate"
+        click_button "Get me a new pirate !"
+      end
+      it "save a new pirate" do
+        expect(Pirate.find_by(name: "new_pirate")).to be_an Pirate
+      end
+      it "display a flash message" do
+        expect(page).to have_content "A new pirate has been add t' th' crew !"
+      end
+    end
+    context "with no name" do
+      it "do not save a new pirate" do
+        click_button "Get me a new pirate !"
+        expect(Pirate.count).to eq 0
+      end
+    end
+    context "with a duplicate name" do
+      before :each do
+        pirate1
+        fill_in :pirate_name, with: pirate1.name
+        click_button "Get me a new pirate !"
+      end
+      it "do not save a new pirate" do
+        expect(Pirate.find_by(name: "new_pirate")).to be nil
+      end
+      it "display a flash message" do
+        expect(page).to have_content "Bloody Davy Jones' locker. 'tis wrong !"
+      end
+    end
   end
 
-  scenario "I can change me bloody pirate details" do
-    pirate1
-    visit root_path
-    click_link "Change th' pirate !"
-    fill_in :pirate_name, with: "edit_name"
-    click_button "Save me pirate"
-    expect(page).to have_content "Pirate's changes has been saved t' rthe registe"
-    expect(page).to have_content "edit_name"
-    expect(Pirate.find_by(name: "edit_name")).to be_an Pirate
+  describe "I edit pirate details" do
+    before(:each) do
+      pirate1
+      visit root_path
+      click_link "Change th' pirate !"
+    end
+
+    context "with valid params" do
+      before :each do
+        fill_in :pirate_name, with: "edit_pirate"
+        click_button "Save me pirate"
+      end
+      it "edit pirate details" do
+        pirate1.reload
+        expect(pirate1.name).to eq "edit_pirate"
+      end
+      it "display a flash message" do
+        expect(page).to have_content "Pirate's changes has been saved t' rthe registe"
+      end
+    end
+
+    context "with no name" do
+      before :each do
+        fill_in :pirate_name, with: ""
+        click_button "Save me pirate"
+      end
+      it "do update pirate details" do
+        pirate1.reload
+        expect(pirate1.name).to_not be ""
+      end
+    end
+
+    context "with a duplicate name" do
+      before :each do
+        fill_in :pirate_name, with: pirate2.name
+        click_button "Save me pirate"
+      end
+      it "do update pirate details" do
+        pirate1.reload
+        expect(pirate1.name).to_not be pirate2.name
+      end
+      it "display a flash message" do
+        expect(page).to have_content "Bloody Davy Jones' locker. 'tis wrong !"
+      end
+    end
   end
 
   scenario "I can scuttle me useless pirate" do
