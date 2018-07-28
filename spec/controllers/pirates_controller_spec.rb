276452533e7fcc0d7156b4d6bfb00d573448f5be
jeremira <jmiraille@gmail.com>
2018-07-28 10:14:52 +0200

    add Pirate controller, routes & spec

---
+++ spec/controllers/pirates_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe PiratesController, type: :controller do
+
+end
---
+++ spec/features/pirates_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.feature "Pirates", type: :feature do
+  let(:pirate1) {create :pirate}
+  let(:pirate2) {create :pirate}
+
+  scenario "I can see all me pirates" do
+    pirate1
+    pirate2
+    visit root_path
+    expect(page).to have_content "AAAARRRHH ! Make me fight"
+    expect(page).to have_content pirate1.name
+    expect(page).to have_content pirate2.name
+  end
+
+  scenario "I can create a new pirate" do
+    visit root_path
+    click_button "Get me a new pirate !"
+    fill_in :name, with: "new_pirate"
+    select 5, from: "hp"
+    select 5, from: "dmg"
+    click_button "Save me pirate"
+    expect(page).to have_content "A new pirate has been add t' th' crew !"
+    expect(Pirate.find_by(name: "new_pirate")).to be_an Pirate
+  end
+
+  scenario "I can change me bloody pirate details" do
+    pirate1
+    visit root_path
+    click_button "change th' pirate !"
+    fill_in :name, with: "edit_name"
+    click_button "Save me pirate"
+    expect(page).to have_content "Pirate's changes has been saved t' rthe registe"
+    expect(page).to have_content "edit_name"
+    expect(Pirate.find_by(name: "edit_pirate")).to be_an Pirate
+  end
+
+  scenario "I can scuttle me useless pirate" do
+    create :pirate, name: "deleted_pirate"
+    visit root_path
+    click_button "scuttle th' pirate !"
+    expect(page).to_not have_content("deleted_pirate")
+    expect(Pirate.find_by(name: "deleted_pirate")).to be nil
+  end
+end
