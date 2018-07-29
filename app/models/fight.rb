14dedc5fabea676d94342d47d87cf91b05c069b1
jeremira <jmiraille@gmail.com>
2018-07-29 15:59:55 +0200

    balance fight algo, community was flamming

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -20,9 +20,9 @@ class Fight < ApplicationRecord
     default = "Nobody win !Ye fork faced panty waist!"
     case self.winner
     when 2
-      return self.pirate_b.name rescue default
+      return self.pirate_b.name.capitalize rescue default
     when 1
-      return self.pirate_a.name rescue default
+      return self.pirate_a.name.capitalize rescue default
     else
       return default
     end
@@ -31,30 +31,30 @@ class Fight < ApplicationRecord
   private
 
   def first_blow
-    # Speed contest to get weapon and first strike
+    # Speed contest to get first strike
     # The less hp and dmg you got the fastest you go
-    a_inertie = 100 - self.pirate_a.hp * self.pirate_a.dmg
-    b_inertie = 100 - self.pirate_b.hp * self.pirate_b.dmg
-    a = rand(1..a_inertie) - b_inertie/2
-    b = rand(1..b_inertie) - a_inertie/2
+    a_inertie = self.pirate_a.hp * self.pirate_a.dmg
+    b_inertie = self.pirate_b.hp * self.pirate_b.dmg
+    a = 150 - rand(0..a_inertie) + b_inertie/3
+    b = 150 - rand(0..b_inertie) - a_inertie/3
     [a, b]
   end
 
   def main_struggle(score)
     # Hp/dmg contest
     a, b = score
-    a_dmg_taken = (self.pirate_b.dmg * (25/(self.pirate_a.hp+1)))
-    b_dmg_taken = (self.pirate_a.dmg * (25/(self.pirate_b.hp+1)))
-    a -= rand(1..a_dmg_taken)
-    b -= rand(1..b_dmg_taken)
+    a_dmg = self.pirate_a.dmg * 10 + (100 - self.pirate_b.hp * 10)
+    b_dmg = self.pirate_b.dmg * 10 + (100 - self.pirate_a.hp * 10)
+    a += rand(0..a_dmg)
+    b += rand(0..b_dmg)
     [a, b]
   end
 
   def last_hope(score)
     # Endurance end of game
     a, b = score
-    a += rand(1..self.pirate_a.hp * 10)
-    b += rand(1..self.pirate_b.hp * 10)
+    a += rand(0..self.pirate_a.hp * 10)
+    b += rand(0..self.pirate_b.hp * 10)
     [a, b]
   end
 
